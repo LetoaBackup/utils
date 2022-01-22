e260cf6fbb242b06e7d139a49aa5456f1c690584
razu <57553476+rqzu@users.noreply.github.com>
2022-01-23 00:35:18 +1000

    added compression to backups

--- src/schemas/Backups.ts
+++ src/schemas/Backups.ts
@@ -2,24 +2,13 @@ import { Schema } from "mongoose";
 
 export const BackupsSchema = new Schema(
     {
-        name: {},
-        region: {},
-        verificationLevel: {},
-        explicitContentFilter: {},
-        defaultMessageNotifications: {},
-        afk: {},
-        widget: {},
-        iconURL: {},
-        bannerURL: {},
-        splashURL: {},
-        channels: { categories: [], others: [] },
-        roles: {},
-        bans: {},
-        emojis: {},
-        createdTimestamp: {},
         guildID: {},
         backup_id: {},
         accountID: { type: String, required: false, default: null },
+        data: {
+            required: false,
+            default: null,
+        },
     },
     { strict: false, strictQuery: false }
 );

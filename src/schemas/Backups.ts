335ad3c1a524274ecd4bec68800f96babfe5a9e0
rqzu <lilwolffy06@gmail.com>
2022-02-03 03:40:24 +1000

    reverted backup schema

--- src/schemas/Backups.ts
+++ src/schemas/Backups.ts
@@ -2,10 +2,24 @@ import { Schema } from "mongoose";
 
 export const BackupsSchema = new Schema(
     {
+        name: {},
+        region: {},
+        verificationLevel: {},
+        explicitContentFilter: {},
+        defaultMessageNotifications: {},
+        afk: {},
+        widget: {},
+        iconURL: {},
+        bannerURL: {},
+        splashURL: {},
+        channels: { categories: [], others: [] },
+        roles: {},
+        bans: {},
+        emojis: {},
+        createdTimestamp: {},
         guildID: {},
         backup_id: {},
         accountID: { type: String, required: false, default: null },
-        data: {},
     },
     { strict: false, strictQuery: false }
 );
